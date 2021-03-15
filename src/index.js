4fe989dc02f6529cc8b93478283ace3a1a9243a6
PaCCiFFisT <pacciffist@gmail.com>
2021-03-15 23:47:51 +0200

    feat: first variant of solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports =
+  function check(str, bracketsConfig) {
+    let solution = [],
+      openBrackets = [],
+      closedBrackets = [];
+    for (let [open, closed] of bracketsConfig) {
+
+      openBrackets.push(open);
+      closedBrackets.push(closed);
+    };
+    for (i = 0; i < str.length; i++) {
+      if ((openBrackets.indexOf(str[i]) >= 0) && (closedBrackets.indexOf(str[i]) != solution[solution.length - 1])) {
+        solution.push(str[i])
+      } else {
+        if (closedBrackets.indexOf(str[i]) != openBrackets.indexOf(solution.pop())) {
+          return false;
+        }
+      }
+    }
+    if (solution.length > 0) {
+      return false;
+    }
+    return true;
+  }
